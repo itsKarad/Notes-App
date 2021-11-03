24af4cc0ab76e4dd4fa29f8a8ee7ecd01f52cb23
Karad <akarad@ee.iitr.ac.in>
2021-11-03 16:20:14 +0530

    Fixed routing

--- src/App.js
+++ src/App.js
@@ -35,36 +35,10 @@ const DUMMY_PAGES = [
 ];
 
 const Pages = ({match}) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [page, setPage] = useState(null);
   const {
     params: {pageId},
   } = match;
-    
-  // This works
-  useEffect(() => {
-    setIsLoading(true);
-    console.log(pageId);
-    const selectedPage = DUMMY_PAGES.find(p => p.id === pageId);
-    console.log(selectedPage);
-    setPage(selectedPage);    
-    setIsLoading(false);
-  }, [pageId]);
 
-  if(isLoading){
-    return (
-      <div className="app-container container">
-        <div className = "row">
-          <div className = "col-4">
-            <SideBar pages = {DUMMY_PAGES}></SideBar> 
-          </div>
-          <div className = "col-8">
-            <h3>Loading...</h3>
-          </div>
-        </div>     
-      </div> 
-    )
-  }
   return (      
     <div className="app-container container">
       <div className = "row">
@@ -72,7 +46,7 @@ const Pages = ({match}) => {
           <SideBar pages = {DUMMY_PAGES}></SideBar> 
         </div>
         <div className = "col-8">
-          <Page page = {page}></Page>
+          <Page></Page>
         </div>
       </div>     
     </div>      
--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,11 +1,48 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
+import { useParams } from 'react-router';
 import './Page.css';
 import Editor from '../Editor/Editor';
 import PageHeader from './PageHeader';
 
+const DUMMY_PAGES = [
+    {
+      id: "home",
+      title: "Home",
+      content : "Just some text for home."
+    },
+    {
+      id: "docs",
+      title: "Documentation",
+      content : "Just some text for docs."
+    },
+    {
+      id: "notes",
+      title: "Meeting Notes",
+      content : "Just some text for meeting notes."
+    },
+    {
+      id: "projects",
+      title: "Projects",
+      content : "Just some text for projects."
+    },
+    {
+      id: "public",
+      title: "Public",
+      content : "Just some text for public."
+    },
+];
+
 const Page = (props) => {
-    const [pageTitle, setPageTitle] = useState(props.page.title);
-    const [input, setInput] = useState(props.page.content);
+    const pageId = useParams().pageId;
+    const [pageTitle, setPageTitle] = useState("");
+    const [input, setInput] = useState("");
+
+    useEffect(() => {
+        const selectedPage = DUMMY_PAGES.find(page => page.id === pageId);        
+        setPageTitle(selectedPage.title);
+        setInput(selectedPage.content);
+    }, [pageId]);
+    
     const inputChangeHandler = (e) => {
         setInput(e.target.value);
     }
